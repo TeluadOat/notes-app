e490e8f51ab9d4d116dfc509a6cb9a4529fbc8d4
Olubodun Adeola <olubodunadeola2000@gmail.com>
2025-12-14 09:42:43 +0100

    Create Protected Route Component

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,9 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+export default function ProtectedRoute() {
+    const { user, loading } = useAuth();
+    if (loading) return <p>Checking session</p>;
+    if (!user) return <Navigate to="/login" />;
+    return <Outlet />;
+}
